778a91250f2fb92c22875d8915b958af7754e992
Shakeer Mohammed <99325059+shakeer7@users.noreply.github.com>
2026-03-04 14:03:54 +0530

    Delete monitoring_module.tf

--- monitoring_module.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_application_insights" "appinsights" {
-  name                = "college-app-insights"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  application_type    = "web"
-}
