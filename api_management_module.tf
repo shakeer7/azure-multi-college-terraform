c869da43abb6c12d599b9d983ddde6c8f13a97d9
Shakeer Mohammed <99325059+shakeer7@users.noreply.github.com>
2026-03-04 14:04:50 +0530

    Delete api_management_module.tf

--- api_management_module.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_api_management" "apim" {
-  name                = "college-apim"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  publisher_name      = "College SaaS"
-  publisher_email     = "admin@college.com"
-  sku_name            = "Developer_1"
-}
